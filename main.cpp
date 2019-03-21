d58b7af5334f4bad45231457405d968cf405d266
Antti Vainikka <antti.jk.vainikka@gmail.com>
2019-03-21 13:45:53 +0200

    Now generates amount specified as argument

--- main.cpp
+++ main.cpp
@@ -3,34 +3,89 @@
 #include <list>
 using namespace std;
 
-list<string> file_to_list(string filename) {
-	ifstream inputstream;
+int line_count(string filename) {
+	// Init variables
 	string line;
-	list<string> names;
-	inputstream.open(filename);
-	if (inputstream.is_open()) {
-		while(getline(inputstream, line)){
-			names.push_back(line);
-		}
-
-		inputstream.close();
+	int count = 0;
+	// Open file
+	ifstream inputstream (filename);
+	// Loop through lines
+	while(getline(inputstream, line)) {
+		count++;
 	}
-	return names;
+	// Close file
+	inputstream.close();
+	return count;
 }
 
-int main() {
-	list<string> firstnames = file_to_list("firstnames.txt");
-	list<string> lastnames = file_to_list("lastnames.txt");
+list<int> generate_random_indexes(int count, int max) {
+	list<int> output;
+	for (int i = 0; i < count; ++i) {
+		output.push_back(rand() % max);
+	}
+	return output;
+}
+
+list<string> generate_names(int count) {
+	list<string> output;
+	int firstnames_count = line_count("firstnames.txt");
+	int lastnames_count = line_count("lastnames.txt");
+	list<int> firstname_indexes = generate_random_indexes(count, max(firstnames_count, 0));
+	list<int> lastname_indexes = generate_random_indexes(count, max(lastnames_count, 0));
+
+	while (firstname_indexes.size() > 0) {
+		string firstname, lastname;
+		int firstname_index = firstname_indexes.back();
+		int lastname_index = lastname_indexes.back();
+		firstname_indexes.pop_back();
+		lastname_indexes.pop_back();
+
+		// Get firstname
+		ifstream filestream ("firstnames.txt");
+		string line;
+		int index = 0;
+		while (getline(filestream, line)) {
+			if (index == firstname_index) {
+				firstname = line;
+				break;
+			}
+			index++;
+		}
+		filestream.close();
+
+		// Get lastname
+		filestream.open("lastnames.txt");
+		index = 0;
+		while (getline(filestream, line)) {
+			if (index == lastname_index) {
+				lastname = line;
+				break;
+			}
+			index++;
+		}
+		filestream.close();
+
+		output.push_back(firstname + '\t' + lastname);
+	}
+
+	return output;
+}
+
+int main(int argc, char *argv[]) {
+	int count = strtol(argv[1], nullptr, 0);
+	list<string> output;
 	
-	list<string>::iterator fn_it;
-	list<string>::iterator ln_it;
-	string firstname, lastname, line;
+	output = generate_names(count);
+
 	ofstream outputstream ("output.txt");
-	for (fn_it = firstnames.begin(); fn_it != firstnames.end(); ++fn_it) {
-		for (ln_it = lastnames.begin(); ln_it != lastnames.end(); ++ln_it) {
-			outputstream << fn_it->data() << '\t' << ln_it->data() << '\n';
+	for (list<string>::iterator i = output.begin(); i != output.end(); ++i) {
+		outputstream << i->data();
+		list<string>::iterator i_copy = i;
+		i_copy++;
+		if (i_copy != output.end()) {
+			outputstream << '\n';
 		}
 	}
-	
+
 	return 0;
 }
